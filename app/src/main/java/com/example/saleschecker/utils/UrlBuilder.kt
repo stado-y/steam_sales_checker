62b8a17f32ae233aa8bf940551f0f7c0ad3d948f
stado-y <psybb228@gmail.com>
2022-02-24 01:42:45 +0200

    add urlbuilder

---
+++ app/src/main/java/com/example/saleschecker/utils/UrlBuilder.kt
@@ -0,0 +1,24 @@
+package com.example.saleschecker.utils
+
+import com.example.saleschecker.BuildConfig
+import com.example.saleschecker.mutual.Constants
+
+object UrlBuilder {
+
+    private var imageCurrentUrlNumber = 0
+    fun getImageUrl(
+        gameId: Int,
+        size: String = Constants.IMAGE_CAPSULE
+    ): String {
+        return when (imageCurrentUrlNumber) {
+            0 -> {
+                imageCurrentUrlNumber = 1
+                "${ BuildConfig.IMAGE_BASE_URL }$gameId/$size"
+            }
+            else -> {
+                imageCurrentUrlNumber = 0
+                "${ BuildConfig.IMAGE_SECONDARY_URL }$gameId/$size"
+            }
+        }
+    }
+}
