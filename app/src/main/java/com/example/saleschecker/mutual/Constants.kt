0fc2d73193e7a67d7f8a088ea46cb4b1d3b44e7f
stado-y <psybb228@gmail.com>
2022-04-14 00:03:19 +0300

    add string constants

--- app/src/main/java/com/example/saleschecker/mutual/Constants.kt
+++ app/src/main/java/com/example/saleschecker/mutual/Constants.kt
@@ -3,4 +3,8 @@ package com.example.saleschecker.mutual
 object Constants {
     const val IMAGE_HEADER = "header_292x136.jpg"
     const val IMAGE_CAPSULE = "capsule_184x69.jpg"
+
+    const val CURRENCY_CODE = "currency_code"
+    const val USER_ID = "user_id"
+    const val DEFAULT_USER_ID: Long = -1
 }
