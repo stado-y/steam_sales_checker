de210d85e7078250ebdbd35b1852988d50eb3b04
stado-y <psybb228@gmail.com>
2022-04-14 22:07:09 +0300

    add okhttpclient for glide

--- app/src/main/java/com/example/saleschecker/mutual/GlideObject.kt
+++ app/src/main/java/com/example/saleschecker/mutual/GlideObject.kt
@@ -2,16 +2,38 @@ package com.example.saleschecker.mutual
 
 import android.content.Context
 import android.widget.ImageView
+import com.bumptech.glide.Glide
 import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.Registry
+import com.bumptech.glide.annotation.Excludes
 import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.integration.okhttp3.OkHttpLibraryGlideModule
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
 import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
 import com.bumptech.glide.request.RequestOptions
 import com.example.saleschecker.R
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import java.io.InputStream
 
 @GlideModule
+@Excludes(OkHttpLibraryGlideModule::class)
 class GlideObject : AppGlideModule() {
+
+    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
+        registry.replace(
+            GlideUrl::class.java,
+            InputStream::class.java,
+            OkHttpUrlLoader.Factory(OkHttpClient.Builder()
+                .addInterceptor(
+                    HttpLoggingInterceptor()
+                        .setLevel(HttpLoggingInterceptor.Level.BODY)
+                ).build())
+        )
+    }
 
     override fun applyOptions(context: Context, builder: GlideBuilder) {
         super.applyOptions(context, builder)
