beb3d6370eca2893008187ab766cc95c63635442
stado-y <psybb228@gmail.com>
2022-05-05 22:28:49 +0300

    add api method & response entities for steam game details query

--- app/src/main/java/com/example/saleschecker/workers/SteamPriceUpdateWorker.kt
+++ app/src/main/java/com/example/saleschecker/workers/SteamPriceUpdateWorker.kt
@@ -4,8 +4,6 @@ import android.content.Context
 import android.util.Log
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
-import androidx.work.ListenableWorker
-import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.example.saleschecker.data.local.EntityConverters
 import com.example.saleschecker.data.local.games.GamesDao
@@ -69,7 +67,7 @@ class SteamPriceUpdateWorker @AssistedInject constructor(
         gameIds: List<Int>,
         countryCode: String
     ): Map<String, SteamResponsePriceUpdate> {
-        return steamApiClient.getAppDetails(
+        return steamApiClient.getAppDetailsBulk(
             gameIds.toCsv(),
             countryCode,
         )
