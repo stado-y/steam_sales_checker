985e039cd47b1f19ce1bd5e163eebb14c6a35b59
stado-y <psybb228@gmail.com>
2022-05-08 01:01:50 +0300

    add position save for recycler in wishlist and home fragments | edit diffutil for gamelist adapter

--- app/src/main/java/com/example/saleschecker/wishlistfragment/WishListFragment.kt
+++ app/src/main/java/com/example/saleschecker/wishlistfragment/WishListFragment.kt
@@ -1,19 +1,22 @@
 package com.example.saleschecker.wishlistfragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Parcelable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.saleschecker.databinding.FragmentWishListBinding
+import com.example.saleschecker.mutual.FragmentWithRecycler
 import com.example.saleschecker.mutual.GameListAdapter
 import com.example.saleschecker.utils.observeWithLifecycle
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class WishListFragment : Fragment() {
+class WishListFragment : FragmentWithRecycler(), BackButtonCallbackReceiver {
 
     lateinit var binding: FragmentWishListBinding
 
@@ -32,14 +35,26 @@ class WishListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        recycler = binding.wishListRecycler
+
         initRecycler()
         observe()
+
+        addBackPressCallback()
+
+        savedInstanceState ?: restoreSavedFromBackButtonCallbackRecyclerState()
     }
 
+    private fun addBackPressCallback() {
+        activity?.onBackPressedDispatcher?.addCallback(
+            viewLifecycleOwner,
+            BackButtonCallback(this),
+        )
 
+    }
 
     private fun initRecycler() {
-        binding.wishListRecycler.apply {
+        recycler?.apply {
             layoutManager = LinearLayoutManager(this.context)
             adapter = gameListAdapter
         }
@@ -47,7 +62,22 @@ class WishListFragment : Fragment() {
     private fun observe() {
         viewModel.games.observeWithLifecycle(viewLifecycleOwner) {
             gameListAdapter.submitList(it)
+            restoreRecyclerState()
         }
     }
 
+    override fun onBackButtonPressed() {
+        backButtonRecyclerSavedState = recycler?.layoutManager?.onSaveInstanceState()
+        Log.e(TAG, "onBackButtonPressed: saving state : $backButtonRecyclerSavedState", )
+        findNavController().popBackStack()
+    }
+
+    private fun restoreSavedFromBackButtonCallbackRecyclerState() {
+        savedRecyclerState = backButtonRecyclerSavedState
+        backButtonRecyclerSavedState = null
+    }
+
+    companion object {
+        internal var backButtonRecyclerSavedState: Parcelable? = null
+    }
 }
